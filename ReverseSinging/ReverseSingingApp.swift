fcee310a3abfe22004402a4949826b5c21e6d948
josepbordes <josepbordesjove@gmail.com>
2025-10-20 20:00:09 +0200

    Initial Commit

---
+++ ReverseSinging/ReverseSingingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReverseSingingApp.swift
+//  ReverseSinging
+//
+//  Created by Josep Bordes Jové on 20/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ReverseSingingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReverseSingingTests/ReverseSingingTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReverseSingingTests.swift
+//  ReverseSingingTests
+//
+//  Created by Josep Bordes Jové on 20/10/25.
+//
+
+import Testing
+@testable import ReverseSinging
+
+struct ReverseSingingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
